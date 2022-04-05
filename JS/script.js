e943665f6e76858ce4a49feef6de4b8de0115a61
dmonteleo <dmonteleo@gmail.com>
2022-04-05 17:07:38 +0200

    prove con javasript

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+console.log('Ciao Mondo');
+console.log('Come Stai?');
+// questo è un commento su un rigo 
+/* commento 
+su 
+più 
+righe */
+console.log(document);
+document.writeln('Hello World');
+alert('Hey tu!');
+document.getElementById('kind-thing').innerHTML+=" questa è la cosa bella";
+console.log(document.getElementById('kind-thing').innerHTML);
+const nome = 'Ugo';
+console.log(nome);
